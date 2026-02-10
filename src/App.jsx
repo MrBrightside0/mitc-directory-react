e0b6b698ecfb82cea473b4ac6d77e40cdf683445
Edmundo <edmundo.morenog@uanl.edu.mx>
2026-02-10 17:24:36 -0600

    Version 1.8: Cambiando clarity por google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { clarity } from 'react-microsoft-clarity';
+
+import ReactGA from 'react-ga4'; 
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import ChatWidget from './components/ui/ChatWidget';
-// 1. IMPORTA EL COMPONENTE
 import ScrollToTop from './components/utils/ScrollToTop'; 
 
 import Directorio from './pages/Directorio';
@@ -15,27 +15,26 @@ import Unirse from './pages/Unirse';
 const App = () => {
 
   useEffect(() => {
-    clarity.init('TU_ID_AQUI'); 
+    // 2. Inicializa Google Analytics con tu ID de Medición (G-XXXXXXXXXX)
+    ReactGA.initialize('TU_ID_DE_GOOGLE_AQUI');
+    
+    
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
   }, []);
 
   return (
     <Router>
       <ScrollToTop />
-
       <div className="flex flex-col min-h-screen bg-white">
-        
         <Navbar />
         <ChatWidget />
-
         <Routes>
           <Route path="/" element={<Inicio />} />
           <Route path="/directorio" element={<Directorio />} />
           <Route path="/unirse" element={<Unirse />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-
         <Footer />
-        
       </div>
     </Router>
   );
